f46f9e4a74cc85b4fdf4b8f256540c326a82a9e7
Olia1515 <lelka703@gmail.com>
2025-10-20 19:12:06 +0300

    lesson2 task1,2

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -15,4 +15,5 @@ const hasTemporaryPass = false
 
 let isAccess
 
-// your code
+isAccess = true;
+if ((isAdmin === true || isVerifiedUser === true) && (hasSpecialPermission === true || hasTemporaryPass === true));// your code
