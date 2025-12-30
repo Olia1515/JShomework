f8e42dbb05fa132bc8346252fb01991efb9555e6
SotnikovaOlga <lelka703@gmail.com>
2025-12-30 12:18:17 +0300

    change1

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,12 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
-}
+  addResource(resource, amount) {
+    if (!game.resources[resource]) {
+      console.log("Invalid resource");
+      return;
+    }
+
+    game.resources[resource] += amount;
+  }
+};
